1edc8fa63c39237c79b9cdffe8760fb29e02b152
Zaccary Price <wildtangomedia@gmail.com>
2013-12-27 22:19:37 +0200

    some initial styles for timeline

---
+++ public/app/scripts/config/route.js
@@ -0,0 +1,16 @@
+/**
+ * Created by vivaldi on 24/12/13.
+ */
+
+
+angular.module('Savings.Config')
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider
+			.when('/timeline', {
+				"controller": "TimelineCtrl",
+				"templateUrl": "views/timeline.html"
+			})
+			.otherwise({
+				"redirectTo": '/timeline'
+			});
+	}]);
