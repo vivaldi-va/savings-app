4094ea8ff159fa6e22088fdb587f6e5b2d09849f
Zaccary Price <wildtangomedia@gmail.com>
2014-02-24 22:23:40 +0200

    User login API. Closes #28

--- controllers/users.js
+++ controllers/users.js
@@ -28,7 +28,7 @@ function _getUserInfo(email) {
 			log.info('DEBUG:', "Get user info result", result);
 
 			if(!result.length) {
-				log.info('ERR:', "No results found for user ", email);
+				log.info('NOTE:', "No results found for user ", email);
 				dfd.reject('NO_RESULTS');
 			}
 
@@ -80,15 +80,70 @@ exports.session = function(req, res) {
 };
 
 exports.login = function(req, res) {
+	var user = {
+		"email": null,
+		"username": null
+	};
+
 	// Validate inputted data
+	var model = {
+		"success": false,
+		"error": null,
+		"message": null,
+		"data": null
+	};
+
+	log.info('DEBUG', "User login info stuffs", req.body);
+
+	// Validate inputted data
+	req.checkBody('email', "Email is missing").notEmpty();
+	req.assert('email', "Invalid email").isEmail();
+
+	req.checkBody('password', "Password is missing").notEmpty();
+	req.assert('password', "Password is too short (min 6 characters)").len(6, 255);
+
+	if(req.validationErrors()) {
+		model.error = req.validationErrors();
+		res.send(model);
+	}
 
 	// check user exists -> grab their info
+	_getUserInfo(req.body.email).then(
+		function(data) {
 
-	// rehash inputted password with stored salt -> compare with stored hash
 
-	// create cookie/cookies
+			log.info('wut wut', data[0]);
+			data = data[0];
+			// rehash inputted password with stored salt -> compare with stored hash
+			//log.info('DEBUG:', "does bcrypt compare sync work?", bcrypt.compareSync(req.body.password, data.passhash));
+			bcrypt.compare(req.body.password, data.passhash, function(err) {
 
-	// done.
+
+				if(err) {
+					log.info('DEBUG:', "pass is bad", err);
+					model.error = "BAD_PASS";
+					res.send(model);
+				}
+
+				user.email 		= req.body.email;
+				user.username 	= data.username;
+
+				// create cookie/cookies
+				res.cookie('saEmail', req.body.email, { maxAge: 60*60*24*180, signed: true });
+
+				model.success 	= true;
+				model.data 		= {"user": user};
+
+				// done.
+				res.send(model);
+			});
+
+		},
+		function(reason) {
+			model.error = reason;
+			res.send(model);
+		}
+	);
 };
 
 exports.logout = function(req, res) {
@@ -152,6 +207,7 @@ exports.register = function(req, res) {
 
 			});
 };
+
 
 exports.resetPass = function(req, res) {
 
