79d50d8545c76fc29189b14d9cf4ff0a19a84062
Zaccary Price <wildtangomedia@gmail.com>
2014-02-25 22:24:30 +0200

    stop crazy errors happening

--- public/app/scripts/services/userservice.js
+++ public/app/scripts/services/userservice.js
@@ -39,7 +39,7 @@ angular.module('Savings.Services')
 				method: 'POST',
 				data: data
 			})
-				.success(function() {
+				.success(function(data) {
 					if(!data.success) {
 						dfd.reject(data.error);
 					}
