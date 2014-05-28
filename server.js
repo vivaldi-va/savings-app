87262b5a613c24122a9f4a03331d6eefdb3da3ed
zac <zaccary.price@avansera.com>
2014-05-28 13:27:27 +0300

    move express config to it's own file, move app index page to public/views and update links accordingly

--- server.js
+++ server.js
@@ -21,53 +21,26 @@ if (cluster.isMaster) {
 
 // Code to run if we're in a worker process
 } else {
-	log.info('Cluster', "Setting up worker worker");
+	log.info('Cluster', "Setting up worker");
 	var express 	= require('express');
 	var mysql 		= require('mysql');
 	var q 			= require('q');
-	var validator	= require('express-validator');
 	var dbConf		= require('./lib/config/conf.json');
 	var app 		= express();
 	var pool		= mysql.createPool(dbConf.db);
 
 
-
-
-
-	/*
-	 * TODO: make express.static point to /app for the actual app stuff, and / point to landing page stuff
-	 * using ejs with html as view engine (google that shit, again...)
-	 * maybe use routes to indicate rendering views, or just use express.static if that works.
-	 */
-	app.configure(function() {
-		app.use(express.cookieParser("gO0g$I3qkEWr0X&C92*P/=aiL8NAV-"));
-		app.use(express.session({secret: "gO0g$I3qkEWr0X&C92*P/=aiL8NAV-"}));
-		app.use(validator());
-		app.set('views', './public');
-		app.engine('html', require('ejs').renderFile);
-		if(process.env.NODE_ENV === 'production') {
-			app.use(express.static(__dirname + 'dist/public')); 	// set the static files location /public/img will be /img for users
-		} else {
-			app.use('/', express.static(__dirname + '/public/landing')); 	// set the static files location /public/img will be /img for users
-			app.use('/app', express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
-		}
-		//log.info('DEBUG:', "static path", __dirname + '/public');
-		app.use(express.logger('dev')); 						// log every request to the console
-		app.use(express.json());
-		app.use(express.urlencoded());							// pull information from html in POST
-		app.use(express.methodOverride()); 						// simulate DELETE and PUT
-	});
-
-	//log.info('DEBUG:', "static path", __dirname + '/public');
-
-	//var controllers = require('./controllers').set(app, db);
-	var routes		= require('./lib/routes')(app);
-	var dbConn		= require('./lib/config/dbConnection')(pool);
+	require('./lib/config/express')(app);
+	require('./lib/routes')(app);
+	require('./lib/config/dbConnection')(pool);
 
 
 	var port = process.env.PORT || 3000;
-	app.listen(port);
-	log.info('SERVER', "Listening on port", port);
+	app.listen(port, function() {
+		log.info('SERVER', "Listening on port", port);
+	});
+
+	exports = module.exports = app;
 }
 
 // Listen for dying workers
