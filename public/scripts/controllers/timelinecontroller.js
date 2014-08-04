4ba193abf3120a455c8b57589b21fdd94a25f216
zaccary price <zaccary.price@avansera.com>
2014-08-04 14:05:28 +0300

    fix timeline balance calculation not working

--- public/scripts/controllers/timelinecontroller.js
+++ public/scripts/controllers/timelinecontroller.js
@@ -9,14 +9,16 @@ angular.module('Savings.Controllers')
 		$rootScope.successes	= [];
 		$scope.timelineBalance	= false;
 
-		$timelineService.getTimeline();
-			/*.then(
+
+		$timelineService.getTimeline()
+			.then(
 				function(success) {
 					$rootScope.successes.push("timeline synced");
 					$log.info("DEBUG: Timeline data received");
 
-					$scope.timeline = success;
-					$scope.timelineBalance = Math.abs(success.attrs.finance_sums.income - success.attrs.finance_sums.expense);
+					//$scope.timeline = success;
+					//$scope.timelineBalance = Math.abs(success.attrs.finance_sums.income - success.attrs.finance_sums.expense);
+					//$scope.timelineBalance = Math.abs($scope.timeline.attrs.finance_sums.income - $scope.timeline.attrs.finance_sums.expense);
 					$timeout(function(){
 						document.getElementById('timeline-scroll').scrollTop = document.getElementById('today').offsetTop-(window.innerHeight/2);
 					});
@@ -26,8 +28,14 @@ angular.module('Savings.Controllers')
 					$log.info("DEBUG: error getting timeline data", reason);
 					$rootScope.errors.push(reason);
 				}
-			);*/
+			);
 
+
+		$rootScope.$watch('timeline', function() {
+			if(!!$rootScope.timeline) {
+				$scope.timelineBalance = Math.abs($scope.timeline.attrs.finance_sums.income - $scope.timeline.attrs.finance_sums.expense);
+			}
+		});
 
 
 	}]);
