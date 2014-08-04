94669d6dbb437f5c9744d6ca3518d877a4cb0d9b
zaccary price <zaccary.price@avansera.com>
2014-08-04 11:48:17 +0300

    add filter to replace number of hours shown for interval

---
+++ public/scripts/filters/interval.filter.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Vivaldi on 04/08/14.
+ */
+
+
+angular.module('Savings.Filters')
+	.filter('interval', function() {
+		return function(hours) {
+			switch(hours) {
+				case 0:
+					return "just the once";
+					break;
+				case 24:
+					return "every day";
+					break;
+				case 168:
+					return "each week";
+					break;
+				case 336:
+					return "every other week";
+					break;
+				case 744:
+					return "every month";
+					break;
+				case 4464:
+					return "every 6 months";
+					break;
+				case 8628:
+					return "once a year";
+					break;
+			}
+		}
+	});
