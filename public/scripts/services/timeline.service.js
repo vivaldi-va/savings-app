aa298651122b3976df9a4b11f65e8d06dc2c133a
vivaldi <wildtangomedia@gmail.com>
2014-09-21 16:41:24 +0100

    updating finances/timeline items updates monthly timeline calculations

--- public/scripts/services/timeline.service.js
+++ public/scripts/services/timeline.service.js
@@ -6,7 +6,69 @@
 
 angular.module('Savings.Services')
 	.factory('$timelineService', function($http, $q, $log, $rootScope, $cookies, SocketService) {
-		function _getEmptyTimeline(past) {
+
+		function _addItemToTimeline(item) {
+
+			var typeString = item.type === 0 ? 'income' : 'expenses';
+
+			angular.forEach($rootScope.timeline.items, function(timelineSegment, key) {
+				var segmentDate	= new Date(timelineSegment.attrs.date);
+				var itemDate	= new Date(item.timeline_date);
+
+
+				var dayMatches	=	segmentDate.getDate() === itemDate.getDate() &&
+					segmentDate.getMonth() === itemDate.getMonth() &&
+					segmentDate.getYear() === itemDate.getYear();
+
+
+				if(dayMatches) {
+					// remove the timeline item from this segment if it exists
+					// to account for the possibility it has been removed
+
+					for(var i = 0; i < timelineSegment.finances[typeString].length; i++) {
+						if(timelineSegment.finances[typeString][i]._id === item._id) {
+							$rootScope.timeline.items[key].finances[typeString].splice(i, 1);
+						}
+					}
+
+					$rootScope.timeline.items[key].finances[typeString].push(item);
+				}
+			});
+
+		}
+
+		function _calcTimelineTotals() {
+			var timeline		= $rootScope.timeline;
+			var incomeTotal		= 0;
+			var expenseTotal	= 0;
+			var itemCount		= 0;
+			var calculatedTotal	= 0;
+
+			angular.forEach(timeline.items, function(segment) {
+				var segmentDate = new Date(segment.attrs.date);
+				if(segmentDate.getMonth() === new Date().getMonth()) {
+
+					angular.forEach(segment.finances.income, function(item) {
+						incomeTotal += item.amount;
+						itemCount++;
+					});
+
+					angular.forEach(segment.finances.expenses, function(item) {
+						expenseTotal += item.amount;
+						itemCount++;
+					});
+				}
+			});
+
+			//calculatedTotal = Math.abs(incomeTotal - expenseTotal);
+
+			$rootScope.timeline.attrs.finance_sums.income = incomeTotal;
+			$rootScope.timeline.attrs.finance_sums.expense = expenseTotal;
+			$rootScope.timeline.attrs.finances_count = itemCount;
+
+		}
+
+		function getEmptyTimeline(past) {
 			$log.info('DEBUG: getTimeline');
 			var dfd = $q.defer();
 			past = past || null;
@@ -38,38 +100,9 @@ angular.module('Savings.Services')
 		}
 
 
-		function _addItemToTimeline(item) {
-
-			var typeString = item.type === 0 ? 'income' : 'expenses';
-
-			angular.forEach($rootScope.timeline.items, function(timelineSegment, key) {
-				var segmentDate	= new Date(timelineSegment.attrs.date);
-				var itemDate	= new Date(item.timeline_date);
 
 
-				var dayMatches	=	segmentDate.getDate() === itemDate.getDate() &&
-									segmentDate.getMonth() === itemDate.getMonth() &&
-									segmentDate.getYear() === itemDate.getYear();
-
-
-				if(dayMatches) {
-					// remove the timeline item from this segment if it exists
-					// to account for the possibility it has been removed
-
-					for(var i = 0; i < timelineSegment.finances[typeString].length; i++) {
-						if(timelineSegment.finances[typeString][i]._id === item._id) {
-							$rootScope.timeline.items[key].finances[typeString].splice(i, 1);
-						}
-					}
-
-					$rootScope.timeline.items[key].finances[typeString].push(item);
-				}
-			});
-
-		}
-
-
-		function _updateTimelineItem(finance, date) {
+		function updateTimelineItem(finance, date) {
 
 			var data = {
 				id: finance._id,
@@ -96,12 +129,13 @@ angular.module('Savings.Services')
 				_transport.on('timeline-item', function(msg) {
 					$log.debug('SOCKET', "Got timeline item", msg.data);
 					_addItemToTimeline(msg.data);
+					_calcTimelineTotals();
 				});
 			}
 		});
 
 		return {
-			getTimeline: _getEmptyTimeline,
-			updateItem: _updateTimelineItem
+			getTimeline: getEmptyTimeline,
+			updateItem: updateTimelineItem
 		};
 	});
