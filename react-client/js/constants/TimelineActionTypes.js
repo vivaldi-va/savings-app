b30f6da136a5a3eeca6d973defe7739116d5cdb3
Zaccary price <wildtangomedia@gmail.com>
2015-05-27 12:15:55 +0100

    TimelineActionTypes was missing apparently

---
+++ react-client/js/constants/TimelineActionTypes.js
@@ -0,0 +1,11 @@
+/**
+ * Created by vivaldi on 07/03/2015.
+ */
+
+var keyMirror = require('react/lib/keyMirror');
+
+// Define action constants
+module.exports = keyMirror({
+	TIMELINE_INIT: null,
+	TIMELINE_ITEM: null
+});
